25d90dc0e8684278b1de26dfc0a5c9ef66dfaf18
benfurber <ben@furber-consultancy.com>
2018-07-11 14:36:18 +0100

    Add method explanation for fizzbuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,6 +1,12 @@
 import pytest
 
 def fizzbuzz(number):
+    '''
+    A standard/simple fizzbuzz for python. It returns the number provided
+    unless a multiple of three (then returns 'fizz'), a multiple of five
+    ('buzz') or both ('fizzbuzz').
+    '''
+    
     if number % 3 == 0 and number % 5 == 0:
         return 'fizzbuzz'
     if number % 3 == 0:
